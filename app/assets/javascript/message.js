c13221a5eaf3e75a0c9d6f31483c8115036c39b8
midoridougan <midori.dougan813@gmail.com>
2019-11-21 20:38:06 +0900

    done_method

--- app/assets/javascript/message.js
+++ app/assets/javascript/message.js
@@ -1,18 +1,24 @@
 $(function(){
   function buildHTML(message){
+    var addImage = (message.image !== null) ? `<img class = "image_size", src="$(message.image)">` : ''
+    
     var html = `
-    .messages
-    .upper-message
-      .upper-message__name
-        = messages.user.name
-      .upper-message__date
-        = messages.created_at.strftime("%Y/%m/%d %H:%M")
-    .lower-message 
-      - if messages.body.present?   
-        .lower-message__message
-          = messages.body
-      =image_tag messages.image.url, class: 'lower-message__image' if messages.image.presen    
-  `
+      <div class="messages">
+        <div class="upper-message">
+          <div class="upper-message__name">
+            ${message.user}
+          </div>
+
+          <div class="upper-message__date">
+            ${message.time}
+          </div>
+        </div>
+        <div class="lower-message">
+          <div class="lower-message__message">
+            ${message.body}
+          </div>
+        </div>
+      </div>`
     return html;
   }
 
@@ -30,10 +36,9 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(message);
-      $('.messages').append(html);
-      $('.textbox').val('');
-      $('.form__submit').prop('disabled', false);
+      var html = buildHTML(data);
+      $('.body').append(html);
+      $('#message_body').val('')
     })
     .fail(function(){
       alert('error');
